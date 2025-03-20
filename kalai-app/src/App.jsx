4fc2311bb7c2e06a16b782b9335b92cd23d181bd
kalai <kalai@cdp360.com>
2025-03-20 20:08:58 +0530

    latest change push

---
+++ kalai-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Kalai from "./Kalai";
+function App() {
+  return <div>
+    intall Comment ( npm create vite@latest kalai-app --template react )
+
+    1.react
+    2.Javascript
+
+
+    3.install vite local
+
+  </div>;
+}
+
+export default App;
